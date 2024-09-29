c8e09cdf03603adb05bc143a3e19272e7e405f26
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2024-09-29 21:02:42 +0200

    6

--- src/ej06.py
+++ src/ej06.py
@@ -1,2 +1,6 @@
 preciototal = int(input('Introduce el precio del articulo:'))
 
+preciosiniva = preciototal/1.10
+iva = preciosiniva * 0.10
+
+print('El precio del articulo sin IVA es:', preciosiniva, 'y el IVA pagado es:', iva)
